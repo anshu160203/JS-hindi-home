553c16b643a518d08d40c3f6dde7b9cef88f265e
anshu160203 <himanshuk1602@gmail.com>
2025-07-13 13:21:43 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,28 @@
+// Datatypes
+
+// "use strict" // treat all js code as newer version of js code
+
+// ECMA Script >> standards of java script >> tc 39...... mdn(for js)
+
+//  data types >> 
+                    // number => 2 to power 53 >> integer, float
+                    // bigInt => when the number is big and is used by big websites eg: facebook
+                    // string => '' , " "
+                    // boolean => true/false
+                    // null => empty, standalone value
+                    // undefined => variable created but value is not define.
+                    // symbol => unique-ness k liye use hota h
+
+// object
+
+// to know the type of any entered value
+console.log(typeof " himanshu");  //string type value
+
+let age = 20
+console.log(typeof age);// number type value
+
+console.log(typeof null);   // object  >>>> null is a object
+console.log(typeof undefined);   // undefined
+
+
+                
