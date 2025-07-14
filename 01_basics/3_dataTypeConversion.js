499ce03f5c41f22869a29d9c95a8c1123ff732da
anshu160203 <himanshuk1602@gmail.com>
2025-07-14 08:10:30 +0000

    datatype conversion

--- 01_basics/3_dataTypeConversion.js
+++ 01_basics/3_dataTypeConversion.js
@@ -1,17 +1,26 @@
 //example
 
 let score = 33
-console.log( typeof score);// we get the value as a number,   but if 33 is written in ""
+console.log( typeof score);// we get the value as a number
 // but if 33 is written in "" 
 let score1 = "33"
 console.log( typeof score1);// we get the value as a string
 // console.log(typeof(any variable));// another way to get type of variable
 
 /* if you get a string > but you are sure that u really want to work on a number not on-
-    a string so you can simply converse the DT*/
+    a string so you can simply converse the DT
+*/
 
-        // METHOD TO CONVERT STRING INTO NUMBER DATA TYPE 
+// METHOD TO CONVERT STRING INTO NUMBER DATA TYPE 
 
 let scoreInNumber = Number(score1) // we take score1 because it gives the string value
+// Data type conversion k time jis DT me change krna hai uska first letter capital format me likhenge
 console.log(typeof scoreInNumber); // and boom we get the value as number
 
+
+// conversion into boolean
+let anshu = 0       // 1=> true; 0=> false; empty string => false; string => true
+let deepak = Boolean(anshu)
+console.log(deepak);
+
+//...................Do all this to convert one data type into another............
