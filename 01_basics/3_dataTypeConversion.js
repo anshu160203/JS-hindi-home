3bc175c17c5640cf9bbef778c1134af09b383e9d
anshu160203 <himanshuk1602@gmail.com>
2025-07-14 08:13:27 +0000

    data type conversion

--- 01_basics/3_dataTypeConversion.js
+++ 01_basics/3_dataTypeConversion.js
@@ -22,5 +22,7 @@ console.log(typeof scoreInNumber); // and boom we get the value as number
 let anshu = 0       // 1=> true; 0=> false; empty string => false; string => true
 let deepak = Boolean(anshu)
 console.log(deepak);
+console.log(typeof deepak);
+
 
 //...................Do all this to convert one data type into another............
