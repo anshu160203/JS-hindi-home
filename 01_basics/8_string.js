ba06b52a8687eb6a088d1dc240910d11df43f7cd
anshu160203 <himanshuk1602@gmail.com>
2025-07-16 11:27:39 +0000

    Strings

---
+++ 01_basics/8_string.js
@@ -0,0 +1,16 @@
+// 1) String concatenate
+let name = "Himanshu"
+let bodyCount = 20
+console.log(name + " have "+ bodyCount + " bodyCount ");// old way
+
+// String inter-polation>>>> in this we use back ticks``
+// ``>>>benefit is that we are making placeholder in these the any variable we make we can direcetly place
+console.log(`hello my name is ${ name } and my body count is ${ bodyCount}`);
+// we can add more strings on the go in string-inter-polation method on placeholder
+
+// one more method to declearing String:-
+
+const myName =  new String ("Himanshu Kumar")// maing new string
+console.log(myName);
+
+
