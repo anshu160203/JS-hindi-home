d1aa2f16e3c210cd8bd2b64c84a7c46b4df27140
anshu160203 <himanshuk1602@gmail.com>
2025-07-17 06:35:36 +0000

    Strings

--- 01_basics/8_string.js
+++ 01_basics/8_string.js
@@ -1,11 +1,11 @@
 //  String concatenate
 let name = "Himanshu"
-let bodyCount = 20
-// console.log(name + " have "+ bodyCount + " bodyCount ");// old way
+let bodyWeight = 70
+// console.log(name + " have "+ bodyWeight + " bodyWeight ");// old way
 
 // String inter-polation>>>> in this we use back ticks``
 // ``>>>benefit is that we are making placeholder in these the any variable we make we can direcetly place
-// console.log(`hello my name is ${ name } and my body count is ${ bodyCount}`);
+// console.log(`hello my name is ${ name } and my body weight is ${ bodyWeight}`);
 // we can add more strings on the go in string-inter-polation method on placeholder
 
 // one more method to declearing String:-
@@ -20,7 +20,20 @@ const myName =  new String ("Himanshu-Kumar")// make new string
 // console.log(myName.charAt(6));// put index and get character
 // console.log(myName.indexOf('s'));// put character and get index
 // Substring
-const newName = myName.substring(3,10) // starting index : 0(included),ending index : 4(excluded)
-console.log(newName);//
+const newName = myName.substring(3,10) // starting index : 3(included),ending index : 10(excluded)
+console.log(newName);//anshu-K bcoz of our input indexes
 
+const sliceString = myName.slice(3,10)//starting index : 3(included),ending index : 10(excluded)
+console.log(sliceString);//anshu-k bcoz of our input indexes.....we can also give -ve values
+
+// str.trim() => remove whitespaces only of starting and ending of string. 
+// str.replace(searchVal, newVal) // search and replace the value.
+
+const url = "https://schoolacademy.com/school%20academy"
+console.log(url.replace('%20', '-'));
+
+// to find any word in string
+console.log(url.includes('school')) // true
+console.log(url.includes('college')) // false
+console.log(myName.split('-'));// return-'Himanshu', 'Kumar' it seperates the string on the basis of "-"
 
